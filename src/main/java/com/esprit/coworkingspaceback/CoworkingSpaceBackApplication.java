605bc3540f47f0dd33e949bd2a8faf16cd8991c8
rouaOunissi <roua.ounissi@esprit.tn>
2024-06-05 09:50:52 +0100

    first commit

---
+++ src/main/java/com/esprit/coworkingspaceback/CoworkingSpaceBackApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.coworkingspaceback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoworkingSpaceBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoworkingSpaceBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esprit/coworkingspaceback/CoworkingSpaceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esprit.coworkingspaceback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoworkingSpaceBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
